a3eae67fbe83584bc57b215b7bf6b55acb6be256
Imran Khan <encrustace@gmail.com>
2025-08-10 22:37:06 +0530

    fixed effect

--- lib/services/visualizer_provider.dart
+++ lib/services/visualizer_provider.dart
@@ -145,7 +145,7 @@ class VisualizerProvider with ChangeNotifier {
         'raiseSpeed': {
           'min': 5.0,
           'max': 30.0,
-          'value': 10,
+          'value': 10.0,
           'steps': 10,
           'default': 10.0,
         },
